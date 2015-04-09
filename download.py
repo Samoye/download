2792c9f96b468a4685959978e682daec1230db30
wyj <samoye@ubuntu.(none)>
2015-04-09 06:29:28 -0700

    a small modify

--- download.py
+++ download.py
@@ -8,17 +8,16 @@ a tool whice can download hot photo in renren.com
 import json
 import urllib
 import os
-print os.path.abspath('.')
-def get_links():
-    req=urllib.urlopen('https://api.renren.com/v2/share/hot/list?access_token=475780|6.bf3041f55a62900ecdeaf9bdaf9a6690.2592000.1431000000-483066716&shareType=TYPE_PHOTO')
-  #  data=json.loads((req.read()).decode('utf-8'))
-    f=open('/home/samoye/python/catch_hot_photo/out','w')
-    f.write(req.read())
-    f.close()
-#    return data
+def get_links():    
+    req=urllib.urlopen('https://api.renren.com/v2/share/hot/list?access_token=475780|6.bf3041f55a62900ecdeaf9bdaf9a6690.2592000.1431000000-483066716&shareType=TYPE_PHOTO') 
+    data=json.loads((req.read()).decode('utf-8'))
+    links=[l['thumbUrl'] for l in data['response']]
+    return links
+    
 
-def download_links(directory,link):
-    image=urllib.urlretrieve(link,directory)
+def download_links(directory,link,name):
+    download_path=os.path.join(directory,name)
+    image=urllib.urlretrieve(link,download_path)
 
 def set_download_dir():
     abspath=os.path.abspath('.')
@@ -27,7 +26,11 @@ def set_download_dir():
         os.mkdir(images_download)
     return images_download
 
-print get_links()
+#url='http://fmn.rrfmn.com/fmn064/20141221/1415/head_BzLf_684300013146118c.jpg'
+#download_dir=set_download_dir()
+#name='%d' % 2+'.jpg'
+#print name
+#download_links(download_dir,url,name)
 #url='http://a2.att.hudong.com/04/58/300001054794129041580438110_950.jpg'
 #dire='/home/samoye/python/catch_hot_photo/2.jpg'
 #download_links(dire,url)
