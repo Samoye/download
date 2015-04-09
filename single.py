ad1f3abad10313044b98f4dafec59a713400d837
wyj <samoye@ubuntu.(none)>
2015-04-09 06:28:12 -0700

    can download

--- single.py
+++ single.py
@@ -1,10 +1,18 @@
 import os
 import json
+import time
 from download import set_download_dir,get_links,download_links
-# download_dir=set_download_dir()
-#    links=[l for l in get_links() if l.endswith('.jpg')]
-f=open('/home/samoye/python/catch_hot_photo/out','r')
-data=json.loads((f.read()).decode('utf-8'))
-f.close()
-links=[l['thumbUrl'] for l in data['response']]
-print links
+def main():
+    download_dir=set_download_dir()
+    links=get_links()
+    i=0
+    print "time0:%s" % time.clock()
+    for link in links:
+        name='%d' % i+'.jpg'
+        download_links(download_dir,link,name)
+ 	i=i+1
+	print "time:%s" % time.clock()
+
+ 
+if __name__=='__main__':
+    main() 
